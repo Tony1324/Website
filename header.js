9f3a1a46226bebd288cd362ba62541db561f3590
TonyZ <tony.j.h.zhang@gmail.com>
2020-12-11 19:40:44 -0500

    small bug fixes with flexbox

--- header.js
+++ header.js
@@ -4,10 +4,8 @@ window.addEventListener("scroll",()=>{
     if(window.scrollY < 50){
         navbar.style.boxShadow = "0px 0px 0px var(--shadow)";
         navbar.style.borderBottom = "0px solid var(--header-secondary)";
-        navbar.style.backgroundColor = "transparent"
     }else {
         navbar.style.boxShadow = "0px 0px 20px var(--shadow)";
         navbar.style.borderBottom = "1px solid var(--header-secondary)";
-        navbar.style.backgroundColor = "var(--header)"
     }
 })
