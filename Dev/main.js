325f57c92a056db6960a5f7974705520815076e3
Tony Zhang <tony.j.h.zhang@gmail.com>
2021-05-12 21:58:56 -0400

    fixed horizontal scrolling

--- Dev/main.js
+++ Dev/main.js
@@ -1,11 +1,13 @@
 window.addEventListener("touchend", (e)=>{
-    document.querySelectorAll(".open").forEach((e)=>{
-        e.classList.remove("open")
-    })
-
-
     if(e.target.closest("section")?.tagName == "SECTION"){
         e.target.closest("section").classList.add("open")
     } 
 })
 
+window.addEventListener("load", ()=>{
+    document.querySelectorAll("section").forEach((e)=>{
+        e.addEventListener("mouseover", ()=>{
+            e.classList.add("open")
+        },{once:true})
+    })
+})
