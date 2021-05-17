5c427f8b48eb6be0e50da1a6a7b0f037da254304
Tony Zhang <tony.j.h.zhang@gmail.com>
2021-05-17 18:26:53 -0400

    more sections and a nice fade in on scroll

--- Dev/main.js
+++ Dev/main.js
@@ -4,3 +4,20 @@ window.addEventListener("resize", ()=>{
         e.style.overflow = "visible" 
     })
 })
+
+window.addEventListener("load", ()=>{
+    document.querySelectorAll("section > *, header > *").forEach((e)=>{
+        console.log(window.innerHeight - e.getBoundingClientRect().top)
+        if(window.innerHeight - e.getBoundingClientRect().top > 140){
+            e.classList.add("scroll-animation-appeared")    
+        }
+    })
+})
+
+window.addEventListener("scroll", (e)=>{
+    document.querySelectorAll("section > *:not(.scroll-animation-appeared), header > *:not(.scroll-animation-appeared)").forEach((e)=>{
+        if(window.innerHeight - e.getBoundingClientRect().top > 140){
+            e.classList.add("scroll-animation-appeared")
+        }
+    })
+})
